10995f077f0c1821b6dd111558d27907cdbd52d2
KrisHoody <hoodykris@gmail.com>
2021-11-16 12:56:24 +0000

    Add functions to print questions. Add score counter and for loop to check throught the questions

--- quiz.py
+++ quiz.py
@@ -6,16 +6,32 @@ print("Welcome, " + x, "You will be presented with 20 Questions. Enter your choi
 score = 0
 name =""
 
-
-
-def quest_answer(all_questions):
-    print()
+   
+def questions(all_questions):
+    global score
     
-    
+    print(all_questions["Question"])
+    print("1", all_questions["1"])
+    print("2", all_questions["2"])
+    print("3", all_questions["3"])
+    print("4", all_questions["4"])
 
-with open("quiz.json") as que_s:
+    guest_answer = input('Your answer:')
+   
+
+    if guest_answer == all_questions["right_answer"]:
+        score += 1
+        print("Great! You've earned a point. You've got:", score, "points" )
+           
+    else:
+        print("Sorry, this is wrong answer")
+        print("The right answer is", all_questions["right_answer"])
+        
+
+with open("quiz_main.json") as que_s:
     all_questions = json.load(que_s)
-
     
     for i in range(0, len(all_questions)):
-        print(all_questions[i]["Question"])
+        questions(all_questions[i])
+
+print("This is the end of quiz. You've got", score, "points. Congratulations")       
